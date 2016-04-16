2c036e7a73d910dfdd673fe96b278f7e085a3b3c
Zach Power <zachpwr@users.noreply.github.com>
2016-04-16 16:49:00 -0400

    added places

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :places
     root "users#index"
     resources :users
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160416175356) do
+ActiveRecord::Schema.define(version: 20160416204559) do
+
+  create_table "places", force: :cascade do |t|
+    t.string   "city"
+    t.string   "state"
+    t.string   "country"
+    t.string   "keywords"
+    t.string   "region"
+    t.string   "wiki"
+    t.string   "language"
+    t.string   "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
